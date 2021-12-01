128c7e4c49036924b976687694fc06f24a16152a
nico-develop <contact@nico-develop.fr>
2021-12-01 08:30:25 +0100

    Updated

--- script-youtube-video.js
+++ script-youtube-video.js
@@ -1,6 +1,8 @@
-const urlSearchParams = new URLSearchParams(window.location.search);
-const params = Object.fromEntries(urlSearchParams.entries());
+window.addEventListener("DOMContentLoaded", (event) => {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
 
-const videoPlayer = document.querySelector("#yt-video");
+  const videoPlayer = document.querySelector("#yt-video");
 
-videoPlayer.setAttribute("data-url", `https://youtu.be/${params.yt}`);
+  videoPlayer.setAttribute("data-url", `https://youtu.be/${params.yt}`);
+});
