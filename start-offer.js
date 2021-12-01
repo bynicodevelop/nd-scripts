cecf1e3c8e8b8000c0cf2e0a5feeaba067cd7cf2
nico-develop <contact@nico-develop.fr>
2021-12-01 09:26:25 +0100

    Updated

---
+++ start-offer.js
@@ -0,0 +1,23 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+const cookieBdOffer = Cookies.get("nd-offer");
+
+Date.prototype.addDays = function (days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
+if (cookieBdOffer == undefined) {
+  const date = new Date();
+
+  Cookies.set(
+    "nd-offer",
+    {
+      id: params.offer,
+      date: date.addDays(params.offerdays),
+    },
+    { expires: 365 }
+  );
+}
