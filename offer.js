6c4e8f3dba70747216bfae784c36447b865a8ddd
nico-develop <contact@nico-develop.fr>
2021-12-01 09:51:51 +0100

    Updated

---
+++ offer.js
@@ -0,0 +1,49 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const cookieBdOffer = Cookies.get("nd-offer");
+
+  document.querySelector("#nd-main-pricing").style.display = "none";
+  document.querySelector("#nd-welcome-flutter-pricing").style.display = "none";
+  document.querySelector("#nd-counter-pricing").style.display = "none";
+
+  const podiaButton = document.querySelector("#podia");
+
+  let podiaScript =
+    '<a href="https://formations.nico-develop.com/flutter-ultimate" data-podia-embed="button" data-text="ACCÉDER À LA FORMATION">ACCÉDER À LA FORMATION</a>';
+
+  if (cookieBdOffer != undefined) {
+    const data = JSON.parse(cookieBdOffer);
+
+    const extratedDate = new Date(Date.parse(data.date));
+
+    var todayDate = extratedDate.toISOString().slice(0, 10);
+
+    var diff =
+      Math.abs(extratedDate.getTime() - new Date().getTime()) / 3600000;
+
+    if (diff < 48) {
+      document.querySelector("#nd-counter-pricing").style.display = "block";
+
+      document
+        .querySelector("#nd-counter-pricing")
+        .setAttribute("data-date", todayDate);
+
+      document
+        .querySelector("#nd-counter-pricing")
+        .setAttribute("data-hour", extratedDate.getHours());
+
+      document
+        .querySelector("#nd-counter-pricing")
+        .setAttribute("data-min", extratedDate.getMinutes());
+    }
+
+    if (Date.parse(data.date) > new Date().getTime()) {
+      document.querySelector("#nd-welcome-flutter-pricing").style.display =
+        "block";
+      podiaScript = `<a href="https://formations.nico-develop.com/flutter-ultimate" data-podia-embed="button" data-coupon="${data.id}" data-text="ACCÉDER À LA FORMATION">ACCÉDER À LA FORMATION</a>`;
+    }
+  } else {
+    document.querySelector("#nd-main-pricing").style.display = "block";
+  }
+
+  podiaButton.innerHTML = podiaScript;
+});
